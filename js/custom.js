8a18d2270b6ffe3cf2a4002dda3e648fdbac76b2
Mat <mathew.teague@yewtreeweb.co.uk>
2017-03-21 00:36:58 +0000

    added sub pages and created recent posts

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+/*-- smooth scrolling for anchor links --*/
+$(function () {
+  $('a.smooth').click(function () {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+/*-- images respinsive --*/
+$(function () {
+  $("img").addClass("img-responsive");
+});
+
+/*-- iframe responsive --*/
+$(function () {
+  $("iframe").addClass("embed-responsive-item");
+  $("iframe").parent().addClass('embed-responsive embed-responsive-16by9');
+});
